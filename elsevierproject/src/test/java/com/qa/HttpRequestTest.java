5af75784af29838ac336c952ef42e47d6b4846e7
Liam <liam@ba.com>
2018-09-18 14:48:28 +0000

    Amended HttpRequestTest to remove errors - currently not working

--- elsevierproject/src/test/java/com/qa/HttpRequestTest.java
+++ elsevierproject/src/test/java/com/qa/HttpRequestTest.java
@@ -1,3 +1,5 @@
+//Cannot get this working as unable to import LocalServerPort dependency
+
 package com.qa;
 
 import org.junit.Test;
@@ -7,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+//import org.springframework.boot.test.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
-	@LocalServerPort
+//	@LocalServerPort
 	private int port;
 	    
 	@Autowired
